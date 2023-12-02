78af72a293d662acde20d229f519bda4b9bafe81
Rayd ivanoff <raydnunes@gmail.com>
2023-12-02 15:17:47 +0000

    Criada todas as stgs

---
+++ models/staging/adventure/stg_sap__customer.sql
@@ -0,0 +1,16 @@
+with
+    fonte_customer as (
+        select 
+        cast(customerid as int) as id_cliente
+        --, cast(personid as tipo) as nome
+        , cast(storeid as int) as id_loja
+        , cast(territoryid as tipo) as nome
+        -- , cast(rowguid as tipo) as nome
+        -- , cast(modifieddate as tipo) as nome
+        from {{ source('sap', 'customer') }}
+    )
+
+select *
+from fonte_customer
+
+
